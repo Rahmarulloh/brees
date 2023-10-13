1af7b807b8a15c0b641fcf329275f50240c4abde
Uzgur <uzbmv2@gmail.com>
2023-10-13 16:22:10 +0500

    init codes

---
+++ src/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+
+const elm = document.getElementById("root") as HTMLElement;
+
+const root = ReactDOM.createRoot(elm);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
