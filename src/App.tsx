5ba87dc89df8722c979639cdf1561e01de6c4d9a
HugoWebDev <mrhuman2709@gmail.com>
2023-10-13 04:34:58 -0700

    styles initialized

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt,
+        placeat veritatis. Mollitia ipsam recusandae omnis necessitatibus
+        pariatur consequuntur, modi unde corrupti nesciunt iure minus, saepe
+        architecto, sunt odit ullam. Ducimus ab minima consequatur voluptates
+        quia itaque eligendi, repellendus ipsa nam a! Odio officiis eligendi
+        neque veritatis possimus ipsam, magnam inventore ex? Cumque similique
+        dolore fuga repudiandae molestias aperiam accusamus, cum numquam
+        deleniti esse officia aliquid sequi, natus maiores provident hic enim
+        possimus inventore officiis tempore quasi autem temporibus! Hic
+        veritatis, consequatur animi esse doloremque est reprehenderit ipsum
+        voluptatibus, enim nostrum odio quod suscipit voluptate. Magnam
+        aspernatur quasi exercitationem laborum fuga?
+      </p>
     </div>
   );
 }
